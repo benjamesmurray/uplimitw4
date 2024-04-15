342125a589b9149484fa0f9ce06db6e7c862157b
Ben Murray <benjamesmurray@gmail.com>
2024-04-15 01:49:17 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,9 @@ room_type = st.radio(
     "What room type do you have?",
     ('Shared room', 'Private room', 'Hotel room', 'Entire home/apt'))
 # service_cost: ['$0.99', '$4.99', '$2.99', '$10.99']
-service_cost = st.radio(
-    "What room type do you have?",
-    ('$0.99', '$4.99', '$2.99', '$10.99'))
+#service_cost = st.radio(
+#    "What room type do you have?",
+#   ('$0.99', '$4.99', '$2.99', '$10.99'))
 # instant_bookable: 0, 1
 instant_bookable = st.radio(
     "Is the listing instantly bookable?",
